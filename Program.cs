4b4aa6ef521842eec3bbf86f6d7c6d116259cbf7
MinKagkag <122770642+MinKagkag@users.noreply.github.com>
2023-03-29 14:22:23 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿class Program {
+ static int Max2(int a, int b) {
+ if (a > b) {
+ return a; 
+ } 
+ return b;
+ }
+ static int Max4(int a, int b, int c, int d) {
+ int result1 = Max2(a, b);
+ int result2 = Max2(c, d);
+ return Max2(result1, result2);
+ }
+}
